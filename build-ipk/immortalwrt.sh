e2a3591eceaa12e63584184c644355e3e8f4e632
Haris131 <41672454+Haris131@users.noreply.github.com>
2023-04-11 09:40:09 +0700

    Update immortalwrt.sh

--- build-ipk/immortalwrt.sh
+++ build-ipk/immortalwrt.sh
@@ -10,17 +10,13 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Clone community packages to package
-[[ -d package ]] && mkdir package
 pushd package
 
 # HelmiWrt packages
-git clone --depth=1 https://github.com/helmiau/helmiwrt-packages
+git clone --depth=1 https://github.com/Haris131/helmiwrt-packages
 
 # Out to openwrt dir
 popd
-
-rm package/helmiwrt-packages/luci-app-libernet-plus/Makefile
-wget https://raw.githubusercontent.com/helmiau/helmiwrt-packages/main/luci-app-libernet-plus/Makefile -O package/helmiwrt-packages/luci-app-libernet-plus/Makefile
 
 #-----------------------------------------------------------------------------
 #   End of @helmiau terminal scripts additionals menu
