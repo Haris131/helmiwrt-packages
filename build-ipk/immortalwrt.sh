3f910b2811e86a1376f587d29a8668107740d466
Haris131 <41672454+Haris131@users.noreply.github.com>
2023-04-20 19:13:29 +0700

    Update immortalwrt.sh

--- build-ipk/immortalwrt.sh
+++ build-ipk/immortalwrt.sh
@@ -13,10 +13,5 @@ pushd package/community
 # HelmiWrt packages
 git clone --depth=1 https://github.com/Haris131/helmiwrt-packages
 
-# kenzo8 small packages
-#svn co https://github.com/kenzok8/small/trunk/trojan-go
-#svn co https://github.com/kenzok8/small/trunk/simple-obfs
-#svn co https://github.com/kenzok8/small/trunk/v2ray-core
-
 # Out to openwrt dir
 popd
