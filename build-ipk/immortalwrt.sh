9520e5814272a7ba51723c85037dac2cd84c0084
Haris131 <41672454+Haris131@users.noreply.github.com>
2025-04-11 12:26:38 +0700

    Update immortalwrt.sh

--- build-ipk/immortalwrt.sh
+++ build-ipk/immortalwrt.sh
@@ -12,7 +12,6 @@ pushd package/community
 
 # HelmiWrt packages
 git clone --depth=1 https://github.com/Haris131/helmiwrt-packages
-rm -rf helmiwrt-packages/v2ray-core
 
 git clone --depth=1 https://github.com/nosignals/openwrt-neko
 sed -i "s|php7|php8|g" openwrt-neko/luci-app-neko/root/etc/neko/core/neko
